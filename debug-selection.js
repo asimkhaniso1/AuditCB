004b89cd89f9a76b83b4c4290305669c834dd4a8
asimkhaniso1 <asimkhaniso1@gmail.com>
2025-12-24 18:53:54 +0500

    Cleanup: Remove debug files and scripts

--- debug-selection.js
+++
@@ -1,7 +0,0 @@
-// Ensure toggleSectionSelection is accessible
-console.log('Checking if toggleSectionSelection exists:', typeof window.toggleSectionSelection);
-if (typeof window.toggleSectionSelection === 'undefined') {
-    console.error('toggleSectionSelection is NOT defined!');
-} else {
-    console.log('toggleSectionSelection is defined and ready');
-}
