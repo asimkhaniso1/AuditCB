15c960609aeef46071fa48a251402565fe0fb1ac
asimkhaniso1 <asimkhaniso1@gmail.com>
2025-12-31 23:59:38 +0500

    Added clients-list.js module for future refactoring

---
+++ clients-list.js
@@ -0,0 +1,10 @@
+// ============================================
+// CLIENTS LIST MODULE (Split from clients-module.js)
+// ============================================
+// This module overrides the list/pagination functions for cleaner code
+// Original functions remain in clients-module.js as fallback
+
+// Note: This file is loaded AFTER clients-module.js
+// Functions defined here will override the originals
+
+Logger.info('Clients List module loaded (split version)');
